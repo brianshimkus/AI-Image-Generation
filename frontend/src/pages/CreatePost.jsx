f7d1be1b421ecba94adb04821144cad407d15d06
Brian Shimkus <brianshimkus1@gmail.com>
2023-01-23 22:36:09 -0600

    CreatePost scaffold

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -11,6 +11,12 @@ export default function CreatePost() {
 	const [generatingImg, setGeneratingImg] = useState(false)
 	const [loading, setLoading] = useState(false)
 
+	const handleSubmit = async (e) => {}
+
+	const handleChange = (e) => {}
+
+	const handleSurpriseMe = () => {}
+
 	return (
 		<section className='max-w-7xl mx-auto'>
 			<div>
@@ -19,6 +25,30 @@ export default function CreatePost() {
 					Create images through AI
 				</p>
 			</div>
+
+			<form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+				<div className='flex flex-col gap-5'>
+					<FormField
+						labelName='Your name'
+						type='text'
+						name='name'
+						placeholder='John Doe'
+						value={form.name}
+						handleChange={handleChange}
+					/>
+
+					<FormField
+						labelName='Prompt'
+						type='text'
+						name='prompt'
+						placeholder='An astronaut lounging in a tropical resort in space, vaporwave'
+						value={form.prompt}
+						handleChange={handleChange}
+						isSurpriseMe
+						handleSurpriseMe={handleSurpriseMe}
+					/>
+				</div>
+			</form>
 		</section>
 	)
 }
