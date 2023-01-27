0200c79a6359eb38166d29deb732143d2b3cc973
Brian Shimkus <brianshimkus1@gmail.com>
2023-01-26 23:55:38 -0600

    More CreatePost page scaffolding

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -48,6 +48,32 @@ export default function CreatePost() {
 						isSurpriseMe
 						handleSurpriseMe={handleSurpriseMe}
 					/>
+					<div className='relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center'>
+						{form.photo ? (
+							<img
+								src={form.photo}
+								alt={form.prompt}
+								className='w-full h-full object-contain'
+							/>
+						) : (
+							<>
+								<svg
+									className='w-8 h-8 text-gray-400'
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+									stroke='currentColor'
+									aria-hidden='true'>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										strokeWidth={2}
+										d='M12 6v6m0 0v6m0-6h6m-6 0H6'
+									/>
+								</svg>
+							</>
+						)}
+					</div>
 				</div>
 			</form>
 		</section>
