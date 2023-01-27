047ea3b6c5cac89a6a8efc19abe16994eb80a8a3
Brian Shimkus <brianshimkus1@gmail.com>
2023-01-26 23:50:13 -0600

    FormField component styling

--- frontend/src/components/FormField.jsx
+++ frontend/src/components/FormField.jsx
@@ -25,6 +25,16 @@ export default function FormField({
 					</button>
 				)}
 			</div>
+			<input
+				type='text'
+				id={name}
+				name={name}
+				placeholder={placeholder}
+				value={value}
+				onChange={handleChange}
+				required
+				className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#649ff] outline-none block w-full p-3'
+			/>
 		</div>
 	)
 }
