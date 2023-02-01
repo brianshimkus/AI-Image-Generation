0a91137de631302ef9b4b84cfd7a4ac6fae2e675
Brian Shimkus <brianshimkus1@gmail.com>
2023-02-01 00:07:52 -0600

    Fixed route paths in backend

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,8 @@ import * as dotenv from 'dotenv'
 import cors from 'cors'
 
 import connectDB from './mongodb/connect.js'
-import postRoutes from './routes/postRoutes.js'
 import imageRoutes from './routes/imageRoutes.js'
+import postRoutes from './routes/postRoutes.js'
 
 dotenv.config()
 
@@ -12,17 +12,19 @@ const app = express()
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
 
-app.use('/api/v1/posts', postRoutes)
+app.use('/api/v1/post', postRoutes)
 app.use('/api/v1/image', imageRoutes)
 
 app.get('/', async (req, res) => {
-	res.send('Hello')
+	res.status(200).json({
+		message: 'Hello',
+	})
 })
 
 const startServer = async () => {
 	try {
 		connectDB(process.env.MONGODB_URL)
-		app.listen(8080, () => console.log('Server has started on port 8080'))
+		app.listen(8080, () => console.log('Server started on port 8080'))
 	} catch (error) {
 		console.log(error)
 	}
