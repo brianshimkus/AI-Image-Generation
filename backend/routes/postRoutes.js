65b1a898c60102bad63a56207bd5b7e68684111c
Brian Shimkus <brianshimkus1@gmail.com>
2023-02-03 23:55:01 -0600

    Create post functionality

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -19,11 +19,31 @@ router.route('/').get(async (req, res) => {
 		const posts = await Post.find({})
 		res.status(200).json({ success: true, data: posts })
 	} catch (err) {
+		res.status(500).json({
+			success: false,
+			message: 'Fetching posts failed, please try again',
+		})
+	}
+})
+
+router.route('/').post(async (req, res) => {
+	try {
+		const { name, prompt, photo } = req.body
+		const photoUrl = await cloudinary.uploader.upload(photo)
+
+		const newPost = await Post.create({
+			name,
+			prompt,
+			photo: photoUrl.url,
+		})
+
+		res.status(200).json({ success: true, data: newPost })
+	} catch (err) {
 		res
 			.status(500)
 			.json({
 				success: false,
-				message: 'Fetching posts failed, please try again',
+				message: 'Unable to create a post, please try again',
 			})
 	}
 })
