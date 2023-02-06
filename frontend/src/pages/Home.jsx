18cc3fd244a435b384228d32cda592d19b6d5ffa
Brian Shimkus <brianshimkus1@gmail.com>
2023-02-05 23:55:11 -0600

    Built out Card component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -75,9 +75,9 @@ export default function Home() {
 						)}
 						<div className='grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-3'>
 							{searchText ? (
-								<RenderCards data={[]} title='No results found' />
+								<RenderCards data={searchedResults} title='No results found' />
 							) : (
-								<RenderCards data={[]} title='No posts found' />
+								<RenderCards data={allPosts} title='No posts found' />
 							)}
 						</div>
 					</>
